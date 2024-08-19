c43b4f43e10bd0922c9151217bbd59c405b8fa5b
Samuel Espinoza Figueroa <82346381+samuel2002Es@users.noreply.github.com>
2024-08-19 17:55:15 +0000

    agrego la inicializacion de firebase en boot

---
+++ src/boot/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBUGrt5pshID-PLs9GI5xo0104jVrfzFTY",
+  authDomain: "trackersap-acabc.firebaseapp.com",
+  projectId: "trackersap-acabc",
+  storageBucket: "trackersap-acabc.appspot.com",
+  messagingSenderId: "440639161701",
+  appId: "1:440639161701:web:671e4f2fae47c44b40cd8c",
+  measurementId: "G-8R4366C47V"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
