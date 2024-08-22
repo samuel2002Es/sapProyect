c73c2dc6b16bd19c5bee1d9c963c04bfb3291e05
Samuel Espinoza Figueroa <82346381+samuel2002Es@users.noreply.github.com>
2024-08-22 21:05:34 +0000

    ya le supe, la cosa es solo hay que hacer un calendario donde obtenga la fecha que quiera ver

--- src/layouts/WorkDays.vue
+++ src/layouts/WorkDays.vue
@@ -7,14 +7,16 @@ import { ref, onMounted } from "vue";
 import { doc, getDocs, collection } from "firebase/firestore";
 import { db } from "src/boot/firebase";
 onMounted(async () => {
-
   try {
     console.log("samuel");
-    const querySnapshot = await getDocs(collection(db, "2025-08-22"));
+    ///WorkDays/August/2025-08-22
+    //const querySnapshot = await getDocs(collection(db, "WorkDays", "MES", "FECHA"));
+    //la collection
+    const querySnapshot = await getDocs(collection(db, "WorkDays"));
     querySnapshot.forEach((doc) => {
-  // doc.data() is never undefined for query doc snapshots
-  console.log(doc.id, " => ", doc.data());
-})
+      // doc.data() is never undefined for query doc snapshots
+      console.log(doc.id, " => ", doc.data());
+    });
   } catch (error) {
     console.log("aqui andamos", error);
   }
