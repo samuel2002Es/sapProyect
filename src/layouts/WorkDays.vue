8a13bcd21d98539534ebd42ab124a9391bf689f8
Samuel Espinoza Figueroa <82346381+samuel2002Es@users.noreply.github.com>
2024-08-22 21:00:47 +0000

    ya lo he conseguido que la api funcione y preguntar por dia

--- src/layouts/WorkDays.vue
+++ src/layouts/WorkDays.vue
@@ -7,14 +7,14 @@ import { ref, onMounted } from "vue";
 import { doc, getDocs, collection } from "firebase/firestore";
 import { db } from "src/boot/firebase";
 onMounted(async () => {
+
   try {
     console.log("samuel");
-    const querySnapshot = await getDocs(collection(db, "WorkDays"));
-    console.log("esto que es", querySnapshot);
+    const querySnapshot = await getDocs(collection(db, "2025-08-22"));
     querySnapshot.forEach((doc) => {
-      // doc.data() is never undefined for query doc snapshots
-      console.log(doc);
-    });
+  // doc.data() is never undefined for query doc snapshots
+  console.log(doc.id, " => ", doc.data());
+})
   } catch (error) {
     console.log("aqui andamos", error);
   }
