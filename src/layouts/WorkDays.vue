c3f3137e187b8c0cbbb0eadef6f8efbc0ae60ce1
samuel Espinoza <samuel2002espinoza@gmail.com>
2024-10-10 17:37:16 -0600

    con excel

--- src/layouts/WorkDays.vue
+++ src/layouts/WorkDays.vue
@@ -1,5 +1,13 @@
 <template>
+  {{ newRows }}
   <div class="q-pa-md">
+    <q-btn
+      class="q-mb-md"
+      @click="descargarExcel"
+      color="white"
+      text-color="black"
+      label="Descargar Data"
+    />
     <div class="text-center">{{ getMonth() }} - {{ getToday(true) }}</div>
     <q-date v-model="days" multiple />
   </div>
@@ -22,10 +30,92 @@
 
 <script setup>
 import { ref, onMounted, watch } from "vue";
+import * as XLSX from "xlsx";
+import { useCounterStore } from "../stores/example-store";
+const store = useCounterStore();
 import { doc, getDocs, collection } from "firebase/firestore";
 import { db } from "src/boot/firebase";
 const days = ref([]);
 const newRows = ref([]);
+const columns = [
+  {
+    name: "day",
+    align: "center",
+    label: "day",
+    field: "day",
+    sortable: true,
+  },
+  {
+    name: "email",
+    align: "center",
+    label: "Email",
+    field: "email",
+    sortable: true,
+  },
+  { name: "initialTime", label: "Initial Day", field: "initialTime" },
+  {
+    name: "finalTime",
+    label: "Final Day",
+    field: "finalTime",
+  },
+  {
+    name: "initiallunch",
+    label: "Initial lunch",
+    field: "initiallunch",
+  },
+  {
+    name: "finallunch",
+    label: "final lunch",
+    field: "finallunch",
+  },
+  {
+    name: "initialbreather",
+    label: "Initial breather",
+    field: "initialbreather",
+  },
+  {
+    name: "finalbreather",
+    label: "Final breather",
+    field: "finalbreather",
+  },
+];
+const labels = columns.map((column) => column.label);
+function descargarExcel() {
+  try {
+    // Crear un nuevo libro de Excel
+    const wb = XLSX.utils.book_new();
+
+    // Crear una hoja de Excel
+    const ws = XLSX.utils.aoa_to_sheet([labels]);
+    const worksheet = XLSX.utils.json_to_sheet(newRows.value);
+
+    // Agregar la hoja al libro
+    XLSX.utils.book_append_sheet(wb, worksheet, "Hoja1");
+
+    // Convertir el libro a un archivo binario
+    const wbout = XLSX.write(wb, { type: "binary", bookType: "xlsx" });
+
+    // Función para convertir un binario a Blob
+    const s2ab = (s) => {
+      const buf = new ArrayBuffer(s.length);
+      const view = new Uint8Array(buf);
+      for (let i = 0; i < s.length; i++) view[i] = s.charCodeAt(i) & 0xff;
+      return buf;
+    };
+
+    // Crear un Blob desde el archivo binario
+    const blob = new Blob([s2ab(wbout)], { type: "application/octet-stream" });
+
+    // Crear un enlace de descarga
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = "archivo_excel.xlsx"; // Nombre del archivo
+    link.click();
+  } catch (error) {
+    console.log(error);
+  }
+}
 watch(days, async () => {
   //console.log("selecciono algo nuevo", days.value);
   //arreglo = days.value.filter((i) => i !== getToday(true)); // filtramos
@@ -203,69 +293,4 @@ onMounted(async () => {
     //console.log("aqui andamos", error);
   }
 });
-const columns = [
-  {
-    name: "day",
-    align: "center",
-    label: "day",
-    field: "day",
-    sortable: true,
-  },
-  {
-    name: "email",
-    align: "center",
-    label: "Email",
-    field: "email",
-    sortable: true,
-  },
-  { name: "initialTime", label: "Initial Day", field: "initialTime" },
-  {
-    name: "finalTime",
-    label: "Final Day",
-    field: "finalTime",
-  },
-  {
-    name: "initiallunch",
-    label: "Initial lunch",
-    field: "initiallunch",
-  },
-  {
-    name: "finallunch",
-    label: "final lunch",
-    field: "finallunch",
-  },
-  {
-    name: "initialbreather",
-    label: "Initial breather",
-    field: "initialbreather",
-  },
-  {
-    name: "finalbreather",
-    label: "Final breather",
-    field: "finalbreather",
-  },
-];
-
-const rows = [
-  {
-    email: "Eclair",
-    finalTime: 262,
-    initialTime: 16.0,
-    carbs: 23,
-    protein: 6.0,
-    sodium: 337,
-    calcium: "6%",
-    iron: "7%",
-  },
-  {
-    email: "Cupcake",
-    finalTime: 305,
-    initialTime: 3.7,
-    carbs: 67,
-    protein: 4.3,
-    sodium: 413,
-    calcium: "3%",
-    iron: "8%",
-  },
-];
 </script>
